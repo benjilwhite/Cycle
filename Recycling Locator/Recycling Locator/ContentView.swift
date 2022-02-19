23912232e54f2dfa572a74bb3c776b5898d72761
Benji White <80803128+benjilwhite@users.noreply.github.com>
2022-02-19 15:58:59 -0800

    Update ContentView.swift

--- Recycling Locator/Recycling Locator/ContentView.swift
+++ Recycling Locator/Recycling Locator/ContentView.swift
@@ -13,16 +13,26 @@ struct ModalView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
-            Text("test menu")
-            Text("Tap tp dismiss")
+        
+        ZStack {
+            Button(action: {presentationMode.wrappedValue.dismiss()}) {
+                Image(systemName: "xmark.circle").padding(10)
+            }.offset(y: -350).font(.system(size: 30))
+            
+            Text("What are you looking to get rid of?").font(.system(size: 22, weight: .regular, design: .serif)).offset(y: -300)
+            
+            Button(action: {
+                      print("button pressed")
+
+                    }) {
+                        Image("Cans")
+                            .resizable().frame(width: 100, height: 100)
+                    }
+            
         }
         .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
         .background(Color.white)
         .edgesIgnoringSafeArea(.all)
-        .onTapGesture {
-            presentationMode.wrappedValue.dismiss()
-        }
     }
 }
 
@@ -42,7 +52,14 @@ struct ContentView: View {
                 
                 Button("Menu") {
                     self.isPresented.toggle()
-                }.offset(y: 340)
+                }
+                .font(.system(size: 40, weight: .heavy, design: .serif))
+                .foregroundColor(.white)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 100)
+                .background(Color.black.opacity(0.3))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .offset(y: 300)
                 .fullScreenCover(isPresented: $isPresented, content: ModalView.init)
             }
         }
@@ -57,36 +74,3 @@ struct ContentView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
-
-
-
-/*class ViewController : UIViewController {
-    let locationManager = CLLocationManager()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
-    }
-}
-
-extension ViewController : CLLocationManagerDelegate {
-    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse {
-            locationManager.requestLocation()
-        }
-    }
-    
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            print("location:: \(location)")
-        }
-    }
-    
-    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        print("error:: \(error)")
-    }
-}
-*/
